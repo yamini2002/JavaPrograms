272a38e060867ec1fdce3d905082b6fc18b0e0f0
unknown <yamiikumar@gmail.com>
2025-04-12 11:47:57 +0530

    Reverse and count of digits code update

--- src/java_programs/CountOfDigits.java
+++ src/java_programs/CountOfDigits.java
@@ -6,13 +6,13 @@ public class CountOfDigits {
 		
 		int num = 27379;
 		int count =0;
-		int temp =0;
+		int temp =num;
 		while(num!=0) {
-		temp = num/10;
-		num = temp;
+		num = num/10;
+		
 		count++;
 		}
-		System.out.println("The count is : " + count);
+		System.out.println("The count of "+temp+ " is : " + count);
 
 	}
 
