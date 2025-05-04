87727dcf37d2fa7e4729f0b4bbc91c9a114968c6
unknown <yamiikumar@gmail.com>
2025-05-04 19:44:03 +0530

    updated PrimeNumber code

--- src/number_solving/PrimeNumber.java
+++ src/number_solving/PrimeNumber.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
 
 public class PrimeNumber {
 
-	public void prime(int start, int end) {
-		for (int i = start; i <= end; i++) {
-			if (i > 1) {
+	public void prime(int num) {
+		
+			if (num > 1) {
 				Boolean prime = true;
-				for (int j = 2; j <= i / 2; j++) {
-					if (i % j == 0) {
+				for (int i = 2; i <= num / 2; i++) {
+					if (num % i == 0) {
 						prime=false;
 						break;
 					}
 				}
 				if (prime) {
-					System.out.println(i+" : is a Prime number");
+					System.out.println(num+" : is a Prime number");
+				}else {
+					System.out.println(num+" : is not a Prime number");
 				}
 			}
 		}
 
-	}
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the first number : ");
 		int a = sc.nextInt();
-		System.out.println("Enter the Second number : ");
-		int b = sc.nextInt();
+	
 		PrimeNumber obj = new PrimeNumber();
-		obj.prime(a, b);
+		obj.prime(a);
 	}
 
 }
